1e46e7f3edb6180872935376e341286a2ed11e51
Kenneth S. Reyes <kenneth.rejes@gmail.com>
2025-08-17 00:55:47 +0200

    further refining

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("Page loaded.")
 });
 
+const currentNumber = 3000;
+
 const container = d3.select("#progress-bar");
 
 const dim = { width: 500, height: 30 };
@@ -26,7 +28,17 @@ svg.append("rect")
 svg.append("rect")
     .attr("x", 0)
     .attr("y", 0)
-    .attr("width", xScale(100 * (3000 / 40000)))
+    .attr("width", xScale(100 * (currentNumber / 50000)))
     .attr("height", dim.height)
-    .style("fill", "#80D39B");
+    .style("fill", "#2c76afff");
+
+svg.append("g")
+    .append("text")
+    .attr("class", "bar-text")
+    .attr("text-anchor", "end")
+    .attr("dominant-baseline", "central")
+    .attr("x", xScale(100 * (currentNumber / 50000)) - 5)
+    .attr("y", dim.height / 2)
+    .text(100 * (currentNumber / 50000) + "%")
+
 
