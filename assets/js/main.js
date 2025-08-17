1a9dbf7b4e0f317fb9c3e3ccab2fa87fa5cb3de1
Kenneth S. Reyes <kenneth.rejes@gmail.com>
2025-08-17 20:38:30 +0200

    update tracking

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("Page loaded.")
 });
 
-const currentNumber = 8000;
+const currentNumber = 11000;
 const targetNumber = 50000;
 
 d3.select(".tracker .current")
